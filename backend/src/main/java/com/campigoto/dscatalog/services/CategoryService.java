90708325c389ee7b41a97d71fc19fe2b78d58b4d
Evandro Campigoto <campigoto@hotmail.com>
2021-11-26 21:52:18 -0300

    H2 database

---
+++ backend/src/main/java/com/campigoto/dscatalog/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.campigoto.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.campigoto.dscatalog.entities.Category;
+import com.campigoto.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	
+	public List<Category> findAll() {
+		return repository.findAll() ;
+	}
+
+}
--- backend/src/test/java/com/campigoto/dscatalog/DscatalogApplicationTests.java
+++ backend/src/test/java/com/campigoto/dscatalog/DscatalogApplicationTests.java
@@ -1,12 +1,8 @@
 package com.campigoto.dscatalog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class DscatalogApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 

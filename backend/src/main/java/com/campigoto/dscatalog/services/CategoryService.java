bce3d69c02d8c829fd2b7a5c73326e6314c78cb2
Evandro Campigoto <campigoto@hotmail.com>
2021-12-01 12:52:53 -0300

    Tratamento de exceções

--- backend/src/main/java/com/campigoto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/campigoto/dscatalog/services/CategoryService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.campigoto.dscatalog.dto.CategoryDTO;
 import com.campigoto.dscatalog.entities.Category;
 import com.campigoto.dscatalog.repositories.CategoryRepository;
+import com.campigoto.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -28,7 +29,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.get();
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException ("Entity not found !"));
 		
 		return new CategoryDTO(entity);
 	}

333d567df72fff199abe1f696e44b4224eec8deb
Evandro Campigoto <campigoto@hotmail.com>
2021-12-03 10:31:17 -0300

    Deletando categoria com DELETE

--- backend/src/main/java/com/campigoto/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/campigoto/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.campigoto.dscatalog.services.exceptions.DatabaseException;
 import com.campigoto.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -15,8 +16,15 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		
-		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Database exception", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databasae(DatabaseException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
 	/*
--- backend/src/main/java/com/campigoto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/campigoto/dscatalog/services/CategoryService.java
@@ -8,12 +8,15 @@ import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.campigoto.dscatalog.dto.CategoryDTO;
 import com.campigoto.dscatalog.entities.Category;
 import com.campigoto.dscatalog.repositories.CategoryRepository;
+import com.campigoto.dscatalog.services.exceptions.DatabaseException;
 import com.campigoto.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -58,6 +61,21 @@ public class CategoryService {
 			throw new ResourceNotFoundException("Id not found " + id);
 			
 		}
+	}
+
+	public void delete(Long id) {
+	
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e ) {
+			throw new ResourceNotFoundException("Id not found " + id);
+			} 
+		catch (DataIntegrityViolationException e ){
+			throw new DatabaseException("Integrity violation");
+					
+		}
+			
 	}
 
 	

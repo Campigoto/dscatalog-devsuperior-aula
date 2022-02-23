5e818382bbd9e8ecb5e8e8b00322b64c21eaf03c
Evandro Campigoto <campigoto@hotmail.com>
2022-02-23 19:40:03 -0300

    Routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,10 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
+import Routes from 'Routes';
 
-function App() {
+const  App = () => {
   return (
-    <Navbar />
+    <Routes />
   )
 }
 
